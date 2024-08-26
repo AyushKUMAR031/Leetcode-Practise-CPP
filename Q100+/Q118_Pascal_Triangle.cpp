fcd398bee0853a3c8166824cecd16d065b1a69e1
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-08-27 00:42:42 +0530

    Q118_Pascal_Triangle (2D-Vectors)

---
+++ Q100+/Q118_Pascal_Triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        for(int i=0;i<numRows;i++){
+            vector<int> arr(i+1);
+            result.push_back(arr);
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    result[i][j]=1;
+                }else{
+                    result[i][j]=result[i-1][j]+result[i-1][j-1];
+                }
+            }
+        }
+        return result;
+    }
+};
