63018441a885b51755a44e944b230397d4fc76a6
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-09-11 17:43:17 +0530

    Q560_SubArray_Sum_Equals_k

---
+++ Q100+/Q560_SubArray_Sum_Equals_k.cpp
@@ -0,0 +1,24 @@
+//my method {{brute force}} time taken - 2080ms
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum = nums[i];
+            if(sum == k){
+                count++;
+            }
+            for(int j=i+1;j<nums.size();j++){
+                sum+=nums[j];
+                if(sum == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//best method
+// ???
