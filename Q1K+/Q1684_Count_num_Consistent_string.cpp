1b6263eb4cbc350c6b61d1fa6a25eaa5fa29d932
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-09-12 10:33:51 +0530

    Q1684_Count_num_Consistent_string

---
+++ Q1K+/Q1684_Count_num_Consistent_string.cpp
@@ -0,0 +1,48 @@
+//my method
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+
+        int count = 0;
+        for(string s : words){
+            bool string_consistent = true;
+            for(char c : s){
+                if(allowed.find(c) == string :: npos){
+                    string_consistent = false;
+                    break;
+                }
+            }
+            if(string_consistent){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//ritesh method
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+
+        ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+        unordered_map<char, int> mp;
+
+        for (auto i : allowed)
+            mp[i] = 1;
+
+        int ans = words.size();
+
+        for (auto i : words) {
+            for (auto j : i) {
+                if (mp[j] == 0) {
+                    ans--;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
