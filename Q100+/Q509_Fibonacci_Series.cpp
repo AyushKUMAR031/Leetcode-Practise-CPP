225a288e81274f8661d933c074da540e7c7e33a7
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-09-21 11:03:28 +0530

    Update Q509_Fibonacci_Series

--- Q100+/Q509_Fibonacci_Series.cpp
+++ Q100+/Q509_Fibonacci_Series.cpp
@@ -15,3 +15,29 @@ public:
     }
 
 };
+
+//by dp recursive and memoization
+class Solution {
+public:
+    int fibo(int n,vector<int> &dp){
+        //step 1
+        if(n <= 1)
+            return n;
+
+        //step 2
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        //step 3
+        dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        int a = fibo(n, dp);
+        return a;
+    }
+};
+
+
