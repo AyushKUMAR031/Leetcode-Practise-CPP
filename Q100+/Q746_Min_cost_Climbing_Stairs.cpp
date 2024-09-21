a15872fe2e91676db19fec189fab9a7fb17b425a
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-09-21 14:30:48 +0530

    Update Q746_Min_cost_Climbing_Stairs

--- Q100+/Q746_Min_cost_Climbing_Stairs.cpp
+++ Q100+/Q746_Min_cost_Climbing_Stairs.cpp
@@ -48,3 +48,22 @@ public:
         return ans;
     }
 };
+
+//space optimization
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        //base case
+        int prev1 = cost[0];
+        int prev2 = cost[1];
+        int curr;
+        //step 1
+        for(int i=2;i<n;i++){
+            curr = min(prev1,prev2) + cost[i];
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return min(prev1,prev2);
+    }
+};
