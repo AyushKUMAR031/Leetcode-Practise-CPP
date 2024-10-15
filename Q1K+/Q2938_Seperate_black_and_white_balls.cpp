c0062fdc60ac41028d9660b1078d244aa80754c0
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-10-15 11:54:17 +0530

    Update Q2938_Seperate_black_and_white_balls

--- Q1K+/Q2938_Seperate_black_and_white_balls.cpp
+++ Q1K+/Q2938_Seperate_black_and_white_balls.cpp
@@ -17,3 +17,23 @@ public:
         return swap_req;
     }
 };
+
+//more better and easier
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long int swap_req = 0;
+        int right_zero = 0;
+        
+        // Iterate from right to left to count necessary swaps
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                right_zero++; // Count zeros on the right
+            } else {
+                // Accumulate swaps needed to move each '1' past all right-side '0's
+                swap_req += right_zero;
+            }
+        }
+        return swap_req;
+    }
+};
