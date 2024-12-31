c54fe332ca84ad81bb9ad27795622a3a6414c2bb
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-12-31 19:36:38 +0530

    Update Q145_PostOrderTraversal.cpp

--- Folder_To_Q0100+/Q145_PostOrderTraversal.cpp
+++ Folder_To_Q0100+/Q145_PostOrderTraversal.cpp
@@ -23,3 +23,44 @@ public:
         return ans;
     }
 };
+
+
+//by two stack
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root,stack<TreeNode*>& s1,stack<TreeNode*>& s2){
+        if(root == NULL) return;
+        s1.push(root);
+        while(!s1.empty()){
+            root = s1.top();
+            s2.push(root);
+            s1.pop();
+            if(root->left != nullptr) s1.push(root->left);
+            if(root->right != nullptr) s1.push(root->right);
+        }
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;
+        solve(root,s1,s2);
+        
+        vector<int> ans;
+
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return ans;
+    }
+};
