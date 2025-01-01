3c8ee24d834fa8233863f59b21a9f65fbce85406
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-01 13:26:16 +0530

    Q78_Subset.cpp
    Recursion | Subset

--- Folder_To_Q0010+/Q78_Subset.cpp
+++ Folder_To_Q0010+/Q78_Subset.cpp
@@ -1,1 +1,26 @@
+//recursion //beats 5.46%
+class Solution {
+public:
+    void solve(vector<int> nums,vector<int> output, int index, vector<vector<int>>& ans){
+        //base case
+        if(index >= nums.size()){
+            ans.push_back(output);
+            return;
+        }
+        //exclude index;
+        solve(nums,output,index+1,ans);
+        //include index;
+        output.push_back(nums[index]);
+        solve(nums,output,index+1,ans);
 
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        solve(nums,output,0,ans);
+        return ans;
+    }
+};
+
+//
