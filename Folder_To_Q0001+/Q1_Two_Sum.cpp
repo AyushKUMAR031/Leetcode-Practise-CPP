0ebd48c59ff261107de94f8129f70a7c6a9c8ecd
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-15 12:39:24 +0530

    Update Q1_Two_Sum.cpp

--- Folder_To_Q0001+/Q1_Two_Sum.cpp
+++ Folder_To_Q0001+/Q1_Two_Sum.cpp
@@ -28,3 +28,24 @@ public:
         return {};
     }
 };
+
+//best
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> temp;
+        for (int i = 0; i < nums.size(); i++) {
+            temp.push_back({nums[i], i});
+        }
+        sort(temp.begin(), temp.end());
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int sum = temp[left].first + temp[right].first;
+            if (sum == target) return {temp[left].second, temp[right].second};
+            if (sum < target) left++;
+            if (sum > target) right--;
+        }
+        return {}; 
+    }
+};
