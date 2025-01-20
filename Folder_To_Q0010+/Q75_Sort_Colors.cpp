16a09e792fece8b531a7f5607d4c848aadb372b8
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-21 00:14:13 +0530

    Create Q75_Sort_Colors.cpp
    Three Pointer | Pointers

---
+++ Folder_To_Q0010+/Q75_Sort_Colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int l = 0,r = nums.size()-1, i = 0;
+       while(i<=r){
+        if(nums[i] == 0){
+            swap(nums[l],nums[i]);
+            l++;
+        }else if(nums[i] == 2){
+            swap(nums[r],nums[i]);
+            r--;
+        }
+        i++;
+       }
+    }
+};
