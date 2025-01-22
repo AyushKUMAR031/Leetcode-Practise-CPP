12028f0a9bad12a3920b0562c3089b9bafc6dc12
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-22 18:08:50 +0530

    Q34_Find_First_&_Last_Occurence
    Binary Search |

--- Folder_To_Q0010+/Q34_Find_First_&_Last_Occurence.cpp
+++ Folder_To_Q0010+/Q34_Find_First_&_Last_Occurence.cpp
@@ -38,4 +38,42 @@ public:
 };
 
 //god logic
+//logic toh woi tha likhne ka tarika alag tha
+class Solution {
+public:
+    void first(vector<int>& nums, int target,int& firstOcc){
+        int s = 0, e = nums.size()-1, mid = s + (e-s)/2;
+        while(s<=e){
+            if(nums[mid] == target){
+                firstOcc = mid;
+                e = mid - 1;
+            }else if(nums[mid] > target){
+                e = mid - 1;
+            }else{
+                s = mid + 1;
+            }
+            mid = s + (e-s)/2;
+        }
+    }
+    void last(vector<int>& nums, int target,int& LastOcc){
+        int s = 0, e = nums.size()-1, mid = s + (e-s)/2;
+        while(s<=e){
+            if(nums[mid] == target){
+                LastOcc = mid;
+                s = mid + 1;
+            }else if(nums[mid] > target){
+                e = mid - 1;
+            }else{
+                s = mid + 1;
+            }
+            mid = s + (e-s)/2;
+        }
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int firstOcc = -1,LastOcc = -1;
+        first(nums,target,firstOcc);
+        last(nums,target,LastOcc);
+        return {firstOcc, LastOcc};
+    }
+};
 
