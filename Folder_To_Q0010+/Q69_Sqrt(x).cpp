bb57db9552c8e891c4cca038e9f287a4c7638d4c
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-23 19:20:23 +0530

    Q69_Sqrt(x)
    Binary Search |

--- Folder_To_Q0010+/Q69_Sqrt(x).cpp
+++ Folder_To_Q0010+/Q69_Sqrt(x).cpp
@@ -28,3 +28,30 @@ public:
         return ans;
     }
 };
+
+
+//better writting
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = 0;
+        if(x==0) return 0;
+        int s = 1,e = x;
+        while(s <= e){
+            long long m = s + (e-s)/2;
+            if(m*m == x){
+                return m;
+            }else if(m*m > x){
+                e = m - 1;
+            }else{
+                //if x is not perfect sqr.
+                // long long check = m+1;
+                // if(check*check > x){
+                //     return m;
+                // }
+                s = m + 1;
+            }
+        }
+        return ans; // we can send directly e for not perfect sqr.
+    }
+};
