7819b9c0bae8b992181c1ed994e946f002096b16
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-25 10:30:31 +0530

    Q876_Middle_In_LL
    LL | Fast & Slow Pointer

--- Folder_To_Q0100+/Q876_Middle_In_LL.cpp
+++ Folder_To_Q0100+/Q876_Middle_In_LL.cpp
@@ -30,3 +30,20 @@ public:
         return m1;
     }
 };
+
+
+//New Trick of Fast and Slow Pointer
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slowPointer = head;
+        ListNode* fastPointer = head;
+
+        while(fastPointer != nullptr && fastPointer->next != nullptr){
+            slowPointer = slowPointer->next;
+            fastPointer = fastPointer->next->next;
+        }
+
+        return slowPointer;
+    }
+};
