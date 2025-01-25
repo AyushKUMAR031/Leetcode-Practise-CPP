077bf8802755aead65a4601f78f482bbc466d271
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-25 10:22:23 +0530

    Q876_Middle_In_LL
    LL |

---
+++ Folder_To_Q0100+/Q876_Middle_In_LL.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* m1 = head;
+        int count = 1;
+        while(m1->next != nullptr){
+            count++;
+            m1 = m1->next;
+        }
+
+        int mid = (count)/2 + 1 ;
+    
+        m1 = head;
+        count = 1;
+        while(count<mid){
+            m1 = m1->next;
+            count++;
+        }
+
+        return m1;
+    }
+};
