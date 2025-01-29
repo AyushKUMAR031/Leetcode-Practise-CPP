40651e07ea7cd4e33f619eae69664101e13cd81d
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-01-29 21:25:15 +0530

    Q2_Add_Two_Numbers
    LL

--- Folder_To_Q0001+/Q2_Add_Two_Numbers.cpp
+++ Folder_To_Q0001+/Q2_Add_Two_Numbers.cpp
@@ -31,3 +31,117 @@ public:
 
     }
 };
+
+
+//best - 100%
+class Solution {
+public:
+    void insertatTail(ListNode* &anshead,ListNode* &ansTail,int digit){
+        ListNode* temp = new ListNode(digit);
+        if(anshead == NULL){
+            anshead = temp;
+            ansTail = temp;
+        }else{
+            ansTail->next = temp;
+            ansTail = temp;
+        }
+    }
+
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry = 0;
+        ListNode* anshead = NULL;
+        ListNode* ansTail = NULL;
+        while(l1 != NULL && l2 != NULL){
+            int sum = carry + l1->val + l2->val;
+            int digit = sum%10;
+            //create node and add it in ans LL
+            insertatTail(anshead,ansTail, digit);
+            carry = sum/10;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+
+        while(l1 != NULL){
+            int sum = carry + l1->val;
+            int digit = sum%10;
+            //create node and add it in ans LL
+            insertatTail(anshead,ansTail, digit);
+            carry = sum/10;
+            l1 = l1->next;
+        }
+
+        while(l2 != NULL){
+            int sum = carry + l2->val;
+            int digit = sum%10;
+            //create node and add it in ans LL
+            insertatTail(anshead,ansTail, digit);
+            carry = sum/10;
+            l2 = l2->next;
+        }
+
+        while(carry != 0){
+            int sum = carry ;
+            int digit = sum%10;
+            //create node and add it in ans LL
+            insertatTail(anshead,ansTail, digit);
+            carry = sum/10;
+        }
+        return anshead;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //adding the number
+        ListNode* ans = add(l1,l2);
+        //bye bye 
+        return ans;
+    }
+};
+
+//more good looking - 100%
+class Solution {
+public:
+    void insertatTail(ListNode* &anshead,ListNode* &ansTail,int digit){
+        ListNode* temp = new ListNode(digit);
+        if(anshead == NULL){
+            anshead = temp;
+            ansTail = temp;
+        }else{
+            ansTail->next = temp;
+            ansTail = temp;
+        }
+    }
+
+    ListNode* add(ListNode* l1, ListNode* l2){
+        int carry = 0;
+        ListNode* anshead = NULL;
+        ListNode* ansTail = NULL;
+
+        while(l1 != NULL || l2 != NULL || carry != 0){
+            int val1 = 0;
+            if(l1 != NULL)
+                val1 = l1->val;
+            int val2 = 0;
+            if(l2 != NULL)
+                val2 = l2->val;
+            
+            int sum = carry + val1 + val2;
+            int digit = sum%10;
+
+            //create node and add it in ans LL
+            insertatTail(anshead,ansTail, digit);
+            carry = sum/10;
+            if(l1 != NULL)
+                l1 = l1->next;
+            if(l2 != NULL)
+                l2 = l2->next;
+        }
+        return anshead;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //adding the number
+        ListNode* ans = add(l1,l2);
+        //bye bye 
+        return ans;
+    }
+};
