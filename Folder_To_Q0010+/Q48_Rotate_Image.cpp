82b95b0d051d1999703dadf1ec9f425b9b385e57
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-05-17 16:24:47 +0530

    Update Q48_Rotate_Image.cpp

--- Folder_To_Q0010+/Q48_Rotate_Image.cpp
+++ Folder_To_Q0010+/Q48_Rotate_Image.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int size = matrix.size(); // as n*n
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) { //Transpose the matrix: convert rows to columns.
             for (int j = i + 1; j < size; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) { // Reverse each row.
             for (int j = 0; j < (size)/2; j++) {
                 swap(matrix[i][j], matrix[i][size - j - 1]);
             }
