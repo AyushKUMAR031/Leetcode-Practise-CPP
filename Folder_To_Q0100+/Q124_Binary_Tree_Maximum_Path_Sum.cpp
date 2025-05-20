f273d07c8414d2f5eb145cf803b2cc460016ab01
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2025-05-20 12:21:53 +0530

    Q124_Binary_Tree_Maximum_Path_Sum.cpp
    Btree

--- Folder_To_Q0100+/Q124_Binary_Tree_Maximum_Path_Sum.cpp
+++ Folder_To_Q0100+/Q124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -1,1 +1,21 @@
 //code
+class Solution {
+public:
+    int maxsum = INT_MIN;
+
+    int solve(TreeNode* root){
+        if( root == NULL) return 0;
+        int left = max(0,solve(root->left)); //comparing with 0 to remove -ve nums.
+        int right = max(0,solve(root->right));
+
+        int currentmax = left + right + root->val;
+        maxsum = max(currentmax, maxsum);
+
+        return root->val + max(left,right);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        solve(root);
+        return maxsum;
+    }
+};
